40ccc39a903138fa7de67048391093a04260b75d
dhywty <clemwek@gmail.com>
2017-09-02 12:27:10 +0300

    Updated to login users

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,43 +3,79 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Navbar from './Navbar';
+import { Redirect } from 'react-router-dom';
+
+const axios = require('axios')
 
 class Login extends Component {
   constructor() {
     super();
     this.state = {
-      style: {
-        margin: 'auto',
-        width: '50%',
-        height: 'auto',
-        textAlign: 'center',
-      }
+      username: '',
+      password: '',
+      login_success: false
+    }
+    this.login = (e) => {
+      e.preventDefault()
+      axios.post('http://127.0.0.1:5000/auth/login', {
+        username: this.state.username,
+        password: this.state.password
+      }).then(resp => {
+        if (resp.status === 202) {
+          this.setState({login_success: true})
+          localStorage.setItem('username', resp.data['username']);
+          localStorage.setItem('token', resp.data['token']);
+          console.log(resp.data)
+        } else {
+          console.log(resp.data)
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
     }
   }
+
   render() {
+    const style = {
+      margin: 'auto',
+      width: '50%',
+      height: 'auto',
+      textAlign: 'center',
+    }
+    if (this.state.login_success) {
+        return <Redirect to='/bucketlist' />
+    }
     return (
       <div className="Login" >
           <Navbar/>
-          <Card style={this.state.style}>
+          <Card style={style}>
             <CardHeader
             title="Login here"
             />
-            <CardText>
-            <TextField
-                hintText="Username"
-                floatingLabelText="Username"
-            /><br />
-                <br />
-            <TextField
-                hintText="Password"
-                floatingLabelText="Password"
-                type="password"
-            /><br />
-            <br />
-            <RaisedButton label="Login" primary={true} />
-            <br />
-            <p>forgot your password?</p>
-            </CardText>
+            <form onSubmit={this.login.bind(this)}>
+              <CardText>
+              <TextField
+                  hintText="Username"
+                  floatingLabelText="Username"
+                  onChange={(e) => {
+                    this.setState({username: e.target.value})
+                  }}
+              /><br />
+                  <br />
+              <TextField
+                  hintText="Password"
+                  floatingLabelText="Password"
+                  type="password"
+                  onChange={(e) => {
+                    this.setState({password: e.target.value})
+                  }}
+              /><br />
+              <br />
+              <RaisedButton type="submit" label="Login" primary={true} />
+              <br />
+              <p>forgot your password?</p>
+              </CardText>
+            </form>
         </Card>
       </div>
     );
