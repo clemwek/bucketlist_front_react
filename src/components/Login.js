e2b3b9af17b6c5689f54964303cdf141ff26bc13
dhywty <clemwek@gmail.com>
2017-09-15 12:17:10 +0300

    Fixed login redirction before setting local storage

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,7 +16,7 @@ class Login extends Component {
       password: '',
       error: '',
       open: false,
-      login_success: false
+      login_success: false,
     }
   }
   login = (e) => {
@@ -25,10 +25,12 @@ class Login extends Component {
       username: this.state.username,
       password: this.state.password
     }).then(resp => {
-        this.setState({login_success: true})
+        console.log('response', resp);
         localStorage.setItem('username', resp.data['username']);
         localStorage.setItem('token', resp.data['token']);
-    }).catch((error) => {
+    })
+    .then(() => this.setState({login_success: true }))
+    .catch((error) => {
       this.setState({error: error.response.data.error})
       this.setState({open: true})
       console.log(error)
@@ -54,48 +56,58 @@ class Login extends Component {
       height: 'auto',
       textAlign: 'center',
     }
+    let login = '';
     if (this.state.login_success) {
-        return <Redirect to='/bucketlist' />
+        login = (
+          <Redirect to='/bucketlist' />
+        );
+    }
+    else if(this.state.login_success === false) {
+      login = (
+        <div className="Login" >
+        <Navbar/>
+        <Card style={style}>
+          <CardHeader
+          title="Login here"
+          />
+          <form onSubmit={this.login.bind(this)}>
+            <CardText>
+            <TextField
+                hintText="Username"
+                floatingLabelText="Username"
+                onChange={(e) => {
+                  this.setState({username: e.target.value})
+                }}
+            />
+            <br />
+                <br />
+            <TextField
+                hintText="Password"
+                floatingLabelText="Password"
+                type="password"
+                onChange={(e) => {
+                  this.setState({password: e.target.value})
+                }}
+            />
+            <br />
+            <br />
+            <RaisedButton type="submit" label="Login" primary={true} />
+            <br />
+            <p>forgot your password?</p>
+            </CardText>
+          </form>
+          <Snackbar
+            open={this.state.open}
+            message={this.state.error}
+            autoHideDuration={4000}
+            onRequestClose={this.handleRequestClose}
+          />
+      </Card>
+    </div>
+      );
     }
     return (
-      <div className="Login" >
-          <Navbar/>
-          <Card style={style}>
-            <CardHeader
-            title="Login here"
-            />
-            <form onSubmit={this.login.bind(this)}>
-              <CardText>
-              <TextField
-                  hintText="Username"
-                  floatingLabelText="Username"
-                  onChange={(e) => {
-                    this.setState({username: e.target.value})
-                  }}
-              /><br />
-                  <br />
-              <TextField
-                  hintText="Password"
-                  floatingLabelText="Password"
-                  type="password"
-                  onChange={(e) => {
-                    this.setState({password: e.target.value})
-                  }}
-              /><br />
-              <br />
-              <RaisedButton type="submit" label="Login" primary={true} />
-              <br />
-              <p>forgot your password?</p>
-              </CardText>
-            </form>
-            <Snackbar
-              open={this.state.open}
-              message={this.state.error}
-              autoHideDuration={4000}
-              onRequestClose={this.handleRequestClose}
-            />
-        </Card>
-      </div>
+      <div>{login}</div>
     );
   }
 }
